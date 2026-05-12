839a9940f705262ae29918a2cef5f3343bb4848d
nithinK-142 <nithin.sagar.359@gmail.com>
2026-05-12 08:19:00 +0530

    update: recover stuck notifications on boot

--- linux-worker/index.js
+++ linux-worker/index.js
@@ -7,12 +7,25 @@ const { processNotification } = require("./util/worker.js");
 
 const { MONGO_URI, DB_NAME } = process.env;
 
+async function recoverStuckNotifications() {
+  const { Notification } = require("./models/notification.model.js");
+  const result = await Notification.updateMany(
+    { status: { $in: ["locked", "processing"] } },
+    { $set: { status: "pending" } }
+  );
+  if (result.modifiedCount > 0) {
+    logWithTimestamp(`[recoverStuckNotifications] Reset ${result.modifiedCount} stuck notification(s) to pending`);
+  }
+}
+
 async function startWorker() {
   logWithTimestamp(`[startWorker] Notification worker booting up`);
 
   await connectDB(MONGO_URI, DB_NAME);
 
   await initFirebase();
+
+  await recoverStuckNotifications();
 
   cron.schedule("*/1 * * * *", processNotification);
 
